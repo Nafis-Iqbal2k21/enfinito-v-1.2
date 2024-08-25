5519b28a310d0d67ea79dce2b30bcfe843f0c0aa
Nafis-Iqbal2k20 <nafisiqbal2k20@gmail.com>
2024-08-26 04:00:53 +0600

    1st correction fix

--- app/services/page.js
+++ app/services/page.js
@@ -3,18 +3,20 @@ import MeetingChat from '@/components/MeetingChat';
 import Reviews from '@/components/Reviews';
 import Services from '@/components/Services';
 import Banner from '@/components/Services/Banner';
+import Slider from '@/components/Slider';
 import WorkProcess from '@/components/WorkProcess';
 import React from 'react'
 
 function page() {
   return (
     <>
-        <Banner />
-        <Services />
-        <WorkProcess />
-        <Reviews />
-        <CollaboratedBrands />
-        <MeetingChat />
+      <Banner />
+      <Slider />
+      <Services />
+      <WorkProcess />
+      <Reviews />
+      <CollaboratedBrands />
+      <MeetingChat />
     </>
   )
 }
